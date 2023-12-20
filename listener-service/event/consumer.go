a3e119cc60b13f90af6517423c47a540cf4e8a25
GustavoMedeiros-A <gustavomedeiros15@outlook.com.br>
2023-12-20 16:58:14 -0300

    feat: start listener service rabbitmq

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,55 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setUp()
+
+	if err != nil {
+		return Consumer{}, err
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) setUp() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(channel)
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (consumer *Consumer) Listen(topics []string) error {
+	ch, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer ch.Close()
+
+	queue, err := declareRandomQueue(ch)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ listener-service/main.go
@@ -0,0 +1,65 @@
+package main
+
+// This package amqp091-go is using because of replace a community developed package
+// and have a lot of people that use that
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// Try to connect to rabbitMQ
+	rabbitConn, err := connect()
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	fmt.Println("We connect to rabbitMQ my Friend...")
+
+	// Start listening for messages
+
+	// Create a consumer
+
+	// Watch the queue and consume events
+
+}
+
+func connect() (*amqp.Connection, error) {
+
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+
+	for {
+		connect, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = connect
+			break
+		}
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+		// Increase the delay
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		fmt.Println("Backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
+
+}
